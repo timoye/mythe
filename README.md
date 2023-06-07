70bb14decf91e088e5db877562238001dc338879
Timothy Soladoye <tim.soladoye@gmail.com>
2023-06-07 16:51:03 +0100

    readme to run test

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Instruction
 
-- Run ```bash run-setup.sh```
+- To Setup ```bash run-setup.sh```
+- To Run Test, after setup, run ```docker-compose exec php php artisan test```
 
 - Access localhost:8080/products
 - Access localhost:8080/products?category=boots&priceLessThan=75000
--- tests/Feature/ProductsAPITest.php
+++ tests/Feature/ProductsAPITest.php
@@ -9,69 +9,7 @@ use Tests\TestCase;
 
 class ProductsAPITest extends TestCase
 {
-    const PRODUCTS = '[
-    {
-        "sku": "000001",
-        "name": "BV Lean leather ankle boots",
-        "category": "boots",
-        "price": {
-            "original": 89000,
-            "final": 62300,
-            "discount_percentage": "30%",
-            "currency": "EUR"
-        }
-    },
-    {
-        "sku": "000003",
-        "name": "Ashlington leather ankle boots",
-        "category": "boots",
-        "price": {
-            "original": 71000,
-            "final": 49700,
-            "discount_percentage": "30%",
-            "currency": "EUR"
-        }
-    },
-    {
-        "sku": "000002",
-        "name": "BV Lean leather ankle boots",
-        "category": "boots",
-        "price": {
-            "original": 99000,
-            "final": 69300,
-            "discount_percentage": "30%",
-            "currency": "EUR"
-        }
-    },
-    {
-        "sku": "000004",
-        "name": "Naima embellished suede sandals",
-        "category": "sandals",
-        "price": {
-            "original": 79500,
-            "final": 79500,
-            "discount_percentage": null,
-            "currency": "EUR"
-        }
-    },
-    {
-        "sku": "000005",
-        "name": "Nathane leather sneakers",
-        "category": "sneakers",
-        "price": {
-            "original": 59000,
-            "final": 59000,
-            "discount_percentage": null,
-            "currencddy": "EUR"
-        }
-    }
-]';
-
-
     public function testProductsResponseHaveAllFields(){
-        Http::fake([
-            '*' => Http::response(self::PRODUCTS, 200),
-        ]);
         $response = $this->get('/products');
         $response->assertStatus(200)
             ->assertJsonStructure([
@@ -91,9 +29,6 @@ class ProductsAPITest extends TestCase
 
     public function testCategoryFilterWorks()
     {
-        Http::fake([
-            '*' => Http::response(self::PRODUCTS, 200),
-        ]);
         $response = $this->get('/products?category=boots');
         $response->assertJsonFragment([
             'category'=>'boots'
